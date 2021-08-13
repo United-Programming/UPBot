e8066933477317031a39d38e22a9e7f3a43538ba
CPULL <claudio.pucci@gmail.com>
2021-08-13 14:57:06 +0200

    🔒️ Removed the token from the code

--- UPBot Code/Program.cs
+++ UPBot Code/Program.cs
@@ -4,18 +4,18 @@ using System.Threading.Tasks;
 namespace UPBot {
   class Program {
     static void Main(string[] args) {
-      MainAsync().GetAwaiter().GetResult();
+      MainAsync(args[0]).GetAwaiter().GetResult();
     }
 
-    static async Task MainAsync() {
+    static async Task MainAsync(string token) {
       var discord = new DiscordClient(new DiscordConfiguration() {
-        Token = "ODc1NzAxNTQ4MzAxMjk5NzQz.YRZWng.2TufJMWrlmo3ZQM9RfnURAS8LUg",
+        Token = token,
         TokenType = TokenType.Bot,
         Intents = DiscordIntents.AllUnprivileged
       });
 
       discord.MessageCreated += async (s, e) => {
-        if (e.Message.Content.ToLower().StartsWith("upbot"))
+        if (e.Message.Content.ToLower().StartsWith("/upbot"))
           await e.Message.RespondAsync("I am alive!");
       };
 
