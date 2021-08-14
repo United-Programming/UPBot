e0c6b2ac7c9b9076d182a344909c39fb1d971305
CPULL <claudio.pucci@gmail.com>
2021-08-14 23:03:01 +0200

    Added better javascript and json detection

--- UPBot Code/Commands/Refactor.cs
+++ UPBot Code/Commands/Refactor.cs
@@ -13,15 +13,25 @@ public class Refactor : BaseCommandModule {
 
   [Command("refactor")]
   public async Task WhoIsCommand(CommandContext ctx) { // Refactors the previous post, if it is code
-    await RefactorCode(ctx, null);
+    await RefactorCode(ctx, null, null);
+  }
+
+  [Command("refactor")]
+  public async Task WhoIsCommand(CommandContext ctx, string language) { // Refactors the previous post, if it is code
+    await RefactorCode(ctx, null, language);
   }
 
   [Command("refactor")]
   public async Task WhoIsCommand(CommandContext ctx, DiscordMember member) { // Refactor the last post of the specified user in the channel
-    await RefactorCode(ctx, member);
+    await RefactorCode(ctx, member, null);
   }
 
-  private async Task<Task<DiscordMessage>> RefactorCode(CommandContext ctx, DiscordMember m) {
+  [Command("refactor")]
+  public async Task WhoIsCommand(CommandContext ctx, DiscordMember member, string language) { // Refactor the last post of the specified user in the channel
+    await RefactorCode(ctx, member, language);
+  }
+
+  private async Task<Task<DiscordMessage>> RefactorCode(CommandContext ctx, DiscordMember m, string language) {
     DiscordChannel c = ctx.Channel;
     DiscordMessage toRefactor = null;
     if (ctx.Message.Reference != null) toRefactor = ctx.Message.Reference.Message;
@@ -52,18 +62,90 @@ public class Refactor : BaseCommandModule {
       }
     }
 
-    // FIXME remove the ``` at begin and end, if any. And the code name after initial ```
-
-    string guessed = "C# <:csharp:831465428214743060>";
+    string guessed = "no one";
+    string best = "";
+    string langEmoji = "";
     int w = weightCs;
-    if (weightCp > w) { guessed = "C++ <:cpp:831465408874676273>"; w = weightCp; }
-    if (weightJs > w) { guessed = "Javascript <:Javascript:876103767068647435>"; w = weightJs; }
-    if (weightJv > w) { guessed = "Java <:java:875852276017815634>"; w = weightJv; }
-    if (weightPy > w) { guessed = "Python <:python:831465381016895500>"; w = weightPy; }
+    if (weightCs > w) { guessed = "<:csharp:831465428214743060> C#"; w = weightCp; best = "cs"; langEmoji = ":CSharp:"; }
+    if (weightCp > w) { guessed = "<:cpp:831465408874676273> C++"; w = weightCp; best = "cpp"; langEmoji = ":CPP:"; }
+    if (weightJs > w) { guessed = "<:Javascript:876103767068647435> Javascript"; w = weightJs; best = "js"; langEmoji = ":Javascript:"; }
+    if (weightJv > w) { guessed = "<:java:875852276017815634> Java"; w = weightJv; best = "java"; langEmoji = ":Java:"; }
+    if (weightPy > w) { guessed = "<:python:831465381016895500> Python"; w = weightPy; best = "python"; langEmoji = ":Python:"; }
+    if (w == 0 && language == null) return ctx.RespondAsync("Nothing to refactor");
 
-    if (w == 0) return ctx.RespondAsync("Nothing to refactor");
+    // Remove the ``` at begin and end, if any. And the code name after initial ```
+    if (code.Length > 3 && code.Substring(0, 3) == "```") {
+      int pos = code.IndexOf('\n');
+      if (pos != -1) code = code.Substring(pos + 1);
+    }
+    if (code.Length > 3 && code.Substring(code.Length - 3, 3) == "```") {
+      code = code.Substring(0, code.Length - 3);
+    }
+    code = code.Trim(' ', '\t', '\r', '\n');
+    language = NormalizeLanguage(language, best);
+    if (language == null)
+      return ctx.RespondAsync("Best guess for the language is: " + guessed + "\nC# = " + weightCs + " C++ = " + weightCp + " Java = " + weightJv + " Javascript = " + weightJs + " Python = " + weightPy);
+    code = toRefactor.Author.Mention + " Replaced with refactored code\n" + "```" + language + "\n" + code + "\n```";
 
-    return ctx.RespondAsync("Refactoring: " + toRefactor.Content.Substring(0, 20) + "...\nC# = " + weightCs + " C++ = " + weightCp + " Java = " + weightJv + " Javascript = " + weightJs + " Python = " + weightPy + " guessed language → " + guessed);
+    if (guessed == "no one" && language != null) {
+      langEmoji = GetLanguageEmoji(language);
+    }
+
+    DiscordMessage replacement = await ctx.Channel.SendMessageAsync(code);
+    DiscordEmoji.TryFromName(ctx.Client, ":AutoRefactored:", out DiscordEmoji autoRefactored);
+    DiscordEmoji.TryFromName(ctx.Client, langEmoji, out DiscordEmoji emoji);
+    try {
+      if (autoRefactored != null) await replacement.CreateReactionAsync(autoRefactored);
+      if (emoji != null) await replacement.CreateReactionAsync(emoji);
+      await toRefactor.DeleteAsync();
+      await ctx.Message.DeleteAsync();
+    } catch (Exception e) {
+      return ctx.RespondAsync("Exception: " + e.Message);
+    }
+    return ctx.RespondAsync("");
+  }
+
+  private string NormalizeLanguage(string language, string best) {
+    if (language == null) return best;
+    language = language.ToLowerInvariant();
+    if (language == "best") return null;
+    if (language == "what") return null;
+    if (language == "whatis") return null;
+    if (language == "analyze") return null;
+    if (language == "analysis") return null;
+    if (language == "c#") return "cs";
+    if (language == "cs") return "cs";
+    if (language == "csharp") return "cs";
+    if (language == "cpp") return "cpp";
+    if (language == "c++") return "cpp";
+    if (language == "java") return "java";
+    if (language == "javascript") return "js";
+    if (language == "jscript") return "js";
+    if (language == "js") return "js";
+    if (language == "json") return "js";
+    if (language == "typescript") return "js";
+    if (language == "phyton") return "python";
+    if (language == "python") return "python";
+    if (language == "py") return "python";
+    return "";
+  }
+  private string GetLanguageEmoji(string language) {
+    language = language.ToLowerInvariant();
+    if (language == "c#") return ":CSharp:";
+    if (language == "cs") return ":CSharp:";
+    if (language == "csharp") return ":CSharp:";
+    if (language == "cpp") return ":CPP:";
+    if (language == "c++") return ":CPP:";
+    if (language == "java") return ":Java:";
+    if (language == "javascript") return ":Javascript:";
+    if (language == "jscript") return ":Javascript:";
+    if (language == "js") return ":Javascript:";
+    if (language == "typescript") return ":Javascript:";
+    if (language == "json") return ":Javascript:";
+    if (language == "phyton") return ":Python:";
+    if (language == "python") return ":Python:";
+    if (language == "py") return ":Python:";
+    return "";
   }
 
   readonly LangKWord[] keywords = {
@@ -118,7 +200,8 @@ public class Refactor : BaseCommandModule {
     new LangKWord{regexp = new Regex("\\[(\\s*[0-9]+\\s*\\,{0,1})+\\s*\\]", RegexOptions.IgnoreCase, TimeSpan.FromSeconds(1)),   wCs = 0, wCp = 0, wJv = 0, wJs = 4, wPy = 5 },
     new LangKWord{regexp = new Regex("\\[(\\s*\"[^\"]*\"\\s*\\,{0,1})+\\s*\\]", RegexOptions.IgnoreCase, TimeSpan.FromSeconds(1)),   wCs = 0, wCp = 0, wJv = 0, wJs = 4, wPy = 5 },
     new LangKWord{regexp = new Regex("while[\\sa-z0-9\\(\\)]+:\\n", RegexOptions.IgnoreCase, TimeSpan.FromSeconds(1)),   wCs = 0, wCp = 0, wJv = 0, wJs = 0, wPy = 5 },
-    new LangKWord{regexp = new Regex("\n\\s*#\\s*[a-z0-9]", RegexOptions.IgnoreCase, TimeSpan.FromSeconds(1)),   wCs = 0, wCp = 0, wJv = 0, wJs = 0, wPy = 6 },
+    new LangKWord{regexp = new Regex("\\s*#\\s*[a-z0-9]", RegexOptions.IgnoreCase, TimeSpan.FromSeconds(1)),   wCs = 0, wCp = 0, wJv = 0, wJs = 0, wPy = 6 },
+    new LangKWord{regexp = new Regex("\\{.+\"{0,1}[a-z0-9_]+\"{0,1}\\s*:\\s*((\".*\")|[0-9\\.]+)\\s*,", RegexOptions.IgnoreCase | RegexOptions.Singleline, TimeSpan.FromSeconds(1)),   wCs = 0, wCp = 0, wJv = 0, wJs = 9, wPy = 0 },
   };
 
   public class LangKWord {
