055156c6013d25c05bec24ef7ecd4817816d0577
CPULL <claudio.pucci@gmail.com>
2021-08-14 23:38:48 +0200

    Added a unprivileged CheckLanguage and a Privileged refactor. Improved java checking

--- UPBot Code/UtilityFunctions.cs
+++ UPBot Code/UtilityFunctions.cs
@@ -1,7 +1,25 @@
-﻿public static class UtilityFunctions
+﻿using DSharpPlus.Entities;
+
+public static class UtilityFunctions
 {
     public static string PluralFormatter(int count, string singular, string plural)
     {
         return count > 1 ? plural : singular;
     }
+
+  public static DiscordEmoji GetEmoji(EmojiEnum emoji) {
+    return null;
+  }
+}
+
+public enum EmojiEnum {
+  Cpp,
+  CSharp,
+  Java,
+  Javascript,
+  Python,
+  UnitedProgramming,
+  Unity,
+  Godot,
+  AutoMeged,
 }
