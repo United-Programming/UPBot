d649dfbc222af753e222f4684687aec176d07e82
Toni Fasth <toni.fasth@hotmail.com>
2021-08-14 13:27:42 +0300

    Added new command Game

---
+++ UPBot Code/Commands/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+/// <summary>
+/// This command implements a basic ping command.
+/// It is mostly for debug reasons.
+/// author: CPU
+/// </summary>
+public class GameModule : BaseCommandModule
+{
+    [Command("game")]
+    public async Task GameCommand(CommandContext ctx)
+    {
+        await ctx.RespondAsync("Available commands: bool");
+    }
+
+
+}
+
+
