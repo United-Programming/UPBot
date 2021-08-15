cbfe5f8622bedf887c4e2f71be2c79df43d7efe7
Duck <61479436+NiceDuck@users.noreply.github.com>
2021-08-15 21:22:37 +0200

    Finalizing Custom Commands
    'CustomCommandsService.cs'
    - Added summary
    - Added comments
    - Added ability to edit a CC with a Discord command
    - Fixed loading a file (it should read the entire content)
    - Added function, which is executed, when calling a command fails. There, I try to execute a custom command if it exists under the specified name.
    - You can try to get a command by it's name with the two recently integrated functions
    - Split functionality in several functions (cleaning up)
    
    'CustomCommand.cs'
    - Changes members' visibility of the 'CustomCommand' class to 'internal'
    - Added functions to this class, responsible for executing the command and editing the command
    - Using the 'ConstructPath' function in the utility functions to construct the path, instead of doing it in this class.
    
    'UtilityFunctions.cs'
    - As already mentioned, I added a 'ConstructPath' function, which constructs a path in the base directory of the current executable with the given file name and file type (suffix) - (see summary)
    
    'Program.cs'
    - Subscribing to event once a command "errors" to call a custom command if necessary
    - Changed code-style of the file (I hope nobody minds)

--- UPBot Code/Commands/CustomCommandsService.cs
+++ UPBot Code/Commands/CustomCommandsService.cs
@@ -7,6 +7,12 @@ using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Interactivity.Extensions;
 
+/// <summary>
+/// Deals with the functionality of loading, adding and removing "Custom Commands"
+/// "Custom Commands" (short: CC) are a way of adding new commands without touching a single line of code
+/// Moderators can add a new Custom Command using a Discord command
+/// These "Custom Commands" will only display a specified text as a callback when someone calls them
+/// </summary>
 public class CustomCommandsService : BaseCommandModule
 {
     private static readonly List<CustomCommand> Commands = new List<CustomCommand>();
@@ -19,7 +25,7 @@ public class CustomCommandsService : BaseCommandModule
     {
         foreach (var name in names)
         {
-            if (DiscordClient.GetCommandsNext().RegisteredCommands.ContainsKey(name))
+            if (DiscordClient.GetCommandsNext().RegisteredCommands.ContainsKey(name)) // Check if there is a command with one of the names already
             {
                 await ErrorCallback(ctx, name);
                 return;
@@ -27,21 +33,15 @@ public class CustomCommandsService : BaseCommandModule
             
             foreach (var cmd in Commands)
             {
-                if (cmd.Names.Contains(name))
+                if (cmd.Names.Contains(name)) // Check if there is already a CC with one of the names
                 {
                     await ErrorCallback(ctx, name);
                     return;
                 }
             }
         }
-        
-        await ctx.RespondAsync($"Please input the content of the CC {names[0]} in one single message. Your next message will count as the content.");
-        string content = string.Empty;
-        await ctx.Message.GetNextMessageAsync(m =>
-        {
-            content = m.Content;
-            return true;
-        });
+
+        string content = await WaitForContent(ctx, names[0]);
         CustomCommand command = new CustomCommand(names, content);
         await WriteToFile(command);
     }
@@ -58,6 +58,50 @@ public class CustomCommandsService : BaseCommandModule
         }
     }
 
+    [Command("editcc")]
+    [Aliases("ccedit")]
+    [RequireRoles(RoleCheckMode.Any, "Mod", "Owner")] // Restrict access to users with the "Mod" or "Owner" role only
+    public async Task EditCommand(CommandContext ctx, string name)
+    {
+        if (File.Exists(name))
+        {
+            string filePath = UtilityFunctions.ConstructPath(name, ".txt");
+            string content = await WaitForContent(ctx, filePath);
+            List<string> oldLines = new List<string>();
+            using (StreamReader sr = File.OpenText(filePath))
+            {
+                string l;
+                string line = string.Empty;
+                while ((l = await sr.ReadLineAsync()) != null)
+                {
+                    line += l;
+                }
+                oldLines.Add(line);
+            }
+
+            if (oldLines.Count >= 2)
+                oldLines[1] = content;
+            else
+            {
+                await ctx.RespondAsync("An error has occured!");
+                return;
+            }
+            
+            await using (StreamWriter sw = File.AppendText(filePath))
+            {
+                foreach(string s in oldLines)
+                    await sw.WriteLineAsync(s);
+            }
+
+            if (TryGetCommand(name, out CustomCommand command))
+                command.EditCommand(name);
+        }
+        else
+        {
+            await ctx.RespondAsync("There is no Custom Command with this name! Please don't use an alias, use the original name!");
+        }
+    }
+
     internal static async Task LoadCustomCommands()
     {
         foreach (string fileName in Directory.GetFiles(System.AppDomain.CurrentDomain.BaseDirectory))
@@ -68,10 +112,26 @@ public class CustomCommandsService : BaseCommandModule
                 if (string.IsNullOrEmpty(names))
                     continue;
 
-                string content = await sr.ReadLineAsync();
+                string content = string.Empty;
+                string c;
+                while ((c = await sr.ReadLineAsync()) != null)
+                {
+                    content += c;
+                }
 
                 CustomCommand cmd = new CustomCommand(names.Split(','), content);
                 Commands.Add(cmd);
+            }
+        }
+    }
+
+    internal static async Task CommandError(CommandsNextExtension extension, CommandErrorEventArgs args)
+    {
+        if (args.Exception is DSharpPlus.CommandsNext.Exceptions.CommandNotFoundException)
+        {
+            if (TryGetCommand(args.Context.Command.Name, out CustomCommand command))
+            {
+                await command.ExecuteCommand(args.Context);
             }
         }
     }
@@ -83,6 +143,7 @@ public class CustomCommandsService : BaseCommandModule
             await using (StreamWriter sw = File.AppendText(command.FilePath))
             {
                 await sw.WriteLineAsync(string.Join(',', command.Names));
+                await sw.WriteLineAsync(command.Content);
             }
         }
     }
@@ -90,5 +151,29 @@ public class CustomCommandsService : BaseCommandModule
     private async Task ErrorCallback(CommandContext ctx, string name)
     {
         await ctx.RespondAsync($"There is already a command containing the alias {name}");
+    }
+
+    private async Task<string> WaitForContent(CommandContext ctx, string name)
+    {
+        await ctx.RespondAsync($"Please input the content of the CC {name} in one single message. Your next message will count as the content.");
+        string content = string.Empty;
+        await ctx.Message.GetNextMessageAsync(m =>
+        {
+            content = m.Content;
+            return true;
+        });
+        
+        return content;
+    }
+
+    private static bool TryGetCommand(string name, out CustomCommand command)
+    {
+        command = GetCommandByName(name);
+        return command != null;
+    }
+
+    private static CustomCommand GetCommandByName(string name)
+    {
+        return Commands.FirstOrDefault(cc => cc.Names.Contains(name));
     }
 }
--- UPBot Code/Program.cs
+++ UPBot Code/Program.cs
@@ -1,40 +1,47 @@
 ﻿using System;
-using DSharpPlus;
-using DSharpPlus.CommandsNext;
 using System.Reflection;
 using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
 using DSharpPlus.Interactivity;
 using DSharpPlus.Interactivity.Enums;
 using DSharpPlus.Interactivity.Extensions;
 
-namespace UPBot {
-  class Program {
-    static void Main(string[] args) {
-      MainAsync(args[0]).GetAwaiter().GetResult();
+namespace UPBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args[0]).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string token)
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = token, // token has to be passed as parameter
+                TokenType = TokenType.Bot, // We are a bot
+                Intents = DiscordIntents.AllUnprivileged // But very limited right now
+            });
+            discord.UseInteractivity(new InteractivityConfiguration()
+            {
+                PollBehaviour = PollBehaviour.KeepEmojis,
+                Timeout = TimeSpan.FromSeconds(30)
+            });
+            CustomCommandsService.DiscordClient = discord;
+
+            UtilityFunctions.InitClient(discord);
+            var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
+            {
+                StringPrefixes = new[] { "\\" } // The backslash will be the command prefix
+            });
+            commands.CommandErrored += CustomCommandsService.CommandError;
+            commands.RegisterCommands(Assembly.GetExecutingAssembly()); // Registers all defined commands
+
+            await CustomCommandsService.LoadCustomCommands();
+            await discord.ConnectAsync(); // Connects and wait forever
+            await Task.Delay(-1);
+        }
     }
-
-    static async Task MainAsync(string token) {
-      var discord = new DiscordClient(new DiscordConfiguration() {
-        Token = token, // token has to be passed as parameter
-        TokenType = TokenType.Bot, // We are a bot
-        Intents = DiscordIntents.AllUnprivileged // But very limited right now
-      });
-      discord.UseInteractivity(new InteractivityConfiguration() 
-      { 
-        PollBehaviour = PollBehaviour.KeepEmojis,
-        Timeout = TimeSpan.FromSeconds(30)
-      });
-      CustomCommandsService.DiscordClient = discord;
-
-      UtilityFunctions.InitClient(discord);
-      var commands = discord.UseCommandsNext(new CommandsNextConfiguration() {
-        StringPrefixes = new[] { "\\" } // The backslash will be the command prefix
-      });
-      commands.RegisterCommands(Assembly.GetExecutingAssembly()); // Registers all defined commands
-
-      await CustomCommandsService.LoadCustomCommands();
-      await discord.ConnectAsync(); // Connects and wait forever
-      await Task.Delay(-1);
-    }
-  }
 }
