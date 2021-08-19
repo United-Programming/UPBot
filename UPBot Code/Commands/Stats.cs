7e6e5f875cd6f4ebfbbfaa6d7c693946443cc3eb
CPULL <claudio.pucci@gmail.com>
2021-08-19 12:28:48 +0200

    Completed bot finalization

--- UPBot Code/Commands/Stats.cs
+++ UPBot Code/Commands/Stats.cs
@@ -29,59 +29,57 @@ public class Stats : BaseCommandModule {
 
 
   [Command("stats")]
-  [Description("Provides server stats for the 4 most used channels (Unity, CSharp, Help 1, and Help 2")]
+  [Description("Provides server stats for specific channels, with no parameters checks the most 4 commons channels")]
+  [Cooldown(1, 60, CooldownBucketType.Channel | CooldownBucketType.User)]
   public async Task DoStats(CommandContext ctx) {
     await GenerateStats(ctx, null, 100);
   }
   [Command("stats")]
-  [Description("Provides server stats for a specific channel")]
-  public async Task DoStats(CommandContext ctx, DiscordChannel channel) {
+  [Description("Provides server stats for specific channels")]
+  [Cooldown(1, 60, CooldownBucketType.Channel | CooldownBucketType.User)]
+  public async Task DoStats(CommandContext ctx, [Description("Specific channel for the stats")] DiscordChannel channel) {
     await GenerateStats(ctx, channel, 100);
   }
   [Command("stats")]
-  [Description("Provides server stats, for a specified chanenl and with the required number of messages")]
-  public async Task DoStats(CommandContext ctx, DiscordChannel channel, int numMessages) {
+  [Description("Provides server stats for specific channels")]
+  [Cooldown(1, 60, CooldownBucketType.Channel | CooldownBucketType.User)]
+  public async Task DoStats(CommandContext ctx, [Description("Specific channel for the stats")] DiscordChannel channel, [Description("Number of messages to check")] int numMessages) {
     await GenerateStats(ctx, channel, numMessages);
   }
   [Command("stats")]
-  [Description("Provides server stats, for a specified chanenl and with the required number of messages")]
-  public async Task DoStats(CommandContext ctx, int numMessages, DiscordChannel channel) {
-    await GenerateStats(ctx, channel, numMessages);
-  }
-  [Command("stats")]
-  [Description("Provides server stats for the 4 most used channels, with the required number of messages")]
-  public async Task DoStats(CommandContext ctx, int numMessages) {
+  [Description("Provides server stats for specific channels")]
+  [Cooldown(1, 60, CooldownBucketType.Channel | CooldownBucketType.User)]
+  public async Task DoStats(CommandContext ctx, [Description("Number of messages to check")] int numMessages) {
     await GenerateStats(ctx, null, numMessages);
   }
 
   public async Task GenerateStats(CommandContext ctx, DiscordChannel channel, int numMessages) {
-    UtilityFunctions.LogUserCommand(ctx);
-
-    DateTime start = DateTime.Now;
-    if (numMessages > 2000) numMessages = 2000;
-
-    ulong[] channelIDs;
-    string[] channelNames;
-    if (channel == null) {
-      channelIDs = defChannelIDs;
-      channelNames = defChannelNames;
-    }
-    else {
-      channelIDs = new ulong[]{ channel.Id };
-      channelNames = new string[] { channel.Name };
-    }
-
-    DiscordGuild g = ctx.Guild;
-    string title = UtilityFunctions.GetEmojiSnowflakeID(UtilityFunctions.GetEmoji(EmojiEnum.UnitedProgramming)) + " United Programming Statistics";
-    string description = " ---- Fetching data 0/" + channelIDs.Length + " ---- Channel " + channelNames[0] + " ---- ";
-    
-    var e = UtilityFunctions.BuildEmbed(title, description, DiscordColor.Black);
-    int fieldPos = e.Fields.Count - 1;
-    DiscordMessage m = await UtilityFunctions.LogEmbed(e, ctx, true);
-
-
-    int step = 0;
+    Utils.LogUserCommand(ctx);
     try {
+      DateTime start = DateTime.Now;
+      if (numMessages > 2000) numMessages = 2000;
+
+      ulong[] channelIDs;
+      string[] channelNames;
+      if (channel == null) {
+        channelIDs = defChannelIDs;
+        channelNames = defChannelNames;
+      }
+      else {
+        channelIDs = new ulong[] { channel.Id };
+        channelNames = new string[] { channel.Name };
+      }
+
+      DiscordGuild g = ctx.Guild;
+      string title = Utils.GetEmojiSnowflakeID(Utils.GetEmoji(EmojiEnum.UnitedProgramming)) + " United Programming Statistics";
+      string description = " ---- Fetching data 0/" + channelIDs.Length + " ---- Channel " + channelNames[0] + " ---- ";
+
+      var e = Utils.BuildEmbed(title, description, DiscordColor.Black);
+      int fieldPos = e.Fields.Count - 1;
+      DiscordMessage m = await Utils.LogEmbed(e, ctx, true);
+
+
+      int step = 0;
       await Task.Delay(120);
       Dictionary<ulong, int> mentioners = new Dictionary<ulong, int>();
       Dictionary<ulong, int> mentioneds = new Dictionary<ulong, int>();
@@ -164,25 +162,20 @@ public class Stats : BaseCommandModule {
 
       double time = (DateTime.Now - start).TotalMilliseconds / 1000;
       if (channelIDs.Length == 1)
-        e.WithFooter("Statistics from channel " + channelNames[0] +  " and " + numMessages + " messages per channel.\nGenerated in " + time.ToString("N1") + " seconds");
+        e.WithFooter("Statistics from channel " + channelNames[0] + " and " + numMessages + " messages per channel.\nGenerated in " + time.ToString("N1") + " seconds");
       else
         e.WithFooter("Statistics from " + channelIDs.Length + " channels and " + numMessages + " messages per channel.\nGenerated in " + time.ToString("N1") + " seconds");
 
+      await m.ModifyAsync(e.Build());
     } catch (Exception ex) {
-
+      await ctx.RespondAsync(Utils.GenerateErrorAnswer("Stats", ex));
     }
-    await m.ModifyAsync(e.Build());
   }
 
   public class Mentioner {
     public ulong member;
     public int num;
-
   }
 
 
-  const ulong idUnityChannel = 830904407540367441ul;
-  const ulong idCSharpChannel = 830904726375628850ul;
-  const ulong idHelp1Channel = 830921265648631878ul;
-  const ulong idHelp2Channel = 830921315657449472ul;
 }
