dad54060cb6ee76bc10c2cf3c57885f0c4115823
CPULL <claudio.pucci@gmail.com>
2021-08-20 16:57:17 +0200

    More control on EmojiForRoles

--- UPBot Code/DataClasses/EmojiForRoleValue.cs
+++ UPBot Code/DataClasses/EmojiForRoleValue.cs
@@ -15,4 +15,30 @@ public class EmojiForRoleValue {
   public string EmojiName { get; set; }
 
   public DiscordRole dRole; // Not in DB
+
+  internal byte GetId() {
+    byte res = 0;
+
+    ulong v = Channel;
+    for (int i = 0; i < 8; i++) {
+      res ^= (byte)(v & 0xff);
+      v >>= 8;
+    }
+    v = Message;
+    for (int i = 0; i < 8; i++) {
+      res ^= (byte)(v & 0xff);
+      v >>= 8;
+    }
+    v = Role;
+    for (int i = 0; i < 8; i++) {
+      res ^= (byte)(v & 0xff);
+      v >>= 8;
+    }
+    v = EmojiId;
+    for (int i = 0; i < 8; i++) {
+      res ^= (byte)(v & 0xff);
+      v >>= 8;
+    }
+    return res;
+  }
 }
