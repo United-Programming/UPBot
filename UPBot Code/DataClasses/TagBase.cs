0078d221fa5f1664fb2de0a424fa2fedc046598f
CPU <claudio.pucci@gmail.com>
2022-06-08 12:31:54 +0200

    Fixed class issue in TagBase (merge problem)

--- UPBot Code/DataClasses/TagBase.cs
+++ UPBot Code/DataClasses/TagBase.cs
@@ -1,27 +1,19 @@
-using System;
-using System.Threading.Tasks; //need them because we storing DateTime for new feature.
-public class TagBase : Entity
-{
-    [Key] public ulong Guild;
-    [Key] public string Topic;
-    public string Alias1;
-    public string Alias2;
-    public string Alias3;
-    [Comment] public string Information;
-    [Comment] public string Author;
-    [Comment] public long ColorOfTheme;
-    [Comment] public DateTime timeOfCreation;
+public class TagBase : Entity {
+  [Key] public ulong Guild;
+  [Key] public string Topic;
+  public string Alias1;
+  public string Alias2;
+  public string Alias3;
+  [Comment] public string Information;
 
-    public TagBase(ulong guild, string topic, string info, string author, int colorOfTheme, DateTime time)
-    {
-        Guild = guild;
-        Topic = topic;
-        Alias1 = null;
-        Alias2 = null;
-        Alias3 = null;
-        Information = info;
-        Author = author;
-        ColorOfTheme = colorOfTheme;
-        timeOfCreation = time;
-    }
+  public TagBase() { }
+
+  public TagBase(ulong guild, string topic, string info) {
+    Guild = guild;
+    Topic = topic;
+    Alias1 = null;
+    Alias2 = null;
+    Alias3 = null;
+    Information = info;
+  }
 }
