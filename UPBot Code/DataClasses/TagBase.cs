7d54f9943e9b09daed5a63fbfceda26c267191b5
J0nathan550 <88881607+J0nathan550@users.noreply.github.com>
2022-07-26 16:47:41 +0300

    Added ability to save image, author icon.

--- UPBot Code/DataClasses/TagBase.cs
+++ UPBot Code/DataClasses/TagBase.cs
@@ -8,22 +8,27 @@ public class TagBase : Entity {
   public string Alias3;
   [Comment] public string Information;
   public string Author;
+  public string AuthorIcon;
   public long ColorOfTheme;
   public DateTime timeOfCreation;
   public string thumbnailLink;
+  public string imageLink;
 
   public TagBase() { }
 
-  public TagBase(ulong guild, string topic, string info, string author, int colorOfTheme, DateTime time, string thumbnailLink) {
-    Guild = guild;
-    Topic = topic;
-    Alias1 = null;
-    Alias2 = null;
-    Alias3 = null;
-    Information = info;
-    Author = author;
-    ColorOfTheme = colorOfTheme;
-    timeOfCreation = time;
-    this.thumbnailLink = thumbnailLink;
-  }
+  public TagBase(ulong guild, string topic, string info, string author, string authorIcon, int colorOfTheme, DateTime time, string thumbnailLink, string imageLink)
+    {
+        Guild = guild;
+        Topic = topic;
+        Alias1 = null;
+        Alias2 = null;
+        Alias3 = null;
+        Information = info;
+        Author = author;
+        AuthorIcon = authorIcon;
+        ColorOfTheme = colorOfTheme;
+        timeOfCreation = time;
+        this.thumbnailLink = thumbnailLink;
+        this.imageLink = imageLink;
+    }
 }
